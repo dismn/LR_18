5e53ea5adcf52fd171d6ef0fadc5e890e6f85c9e
admin <admin@DESKTOP-GARPRVJ>
2023-04-13 20:52:05 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace LR_18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtArray = new System.Windows.Forms.TextBox();
+            this.txtSum = new System.Windows.Forms.TextBox();
+            this.txtProduct = new System.Windows.Forms.TextBox();
+            this.txtSortedArray = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtArray
+            // 
+            this.txtArray.Location = new System.Drawing.Point(232, 61);
+            this.txtArray.Name = "txtArray";
+            this.txtArray.Size = new System.Drawing.Size(190, 22);
+            this.txtArray.TabIndex = 0;
+            // 
+            // txtSum
+            // 
+            this.txtSum.Location = new System.Drawing.Point(230, 168);
+            this.txtSum.Name = "txtSum";
+            this.txtSum.Size = new System.Drawing.Size(192, 22);
+            this.txtSum.TabIndex = 1;
+            // 
+            // txtProduct
+            // 
+            this.txtProduct.Location = new System.Drawing.Point(232, 122);
+            this.txtProduct.Name = "txtProduct";
+            this.txtProduct.Size = new System.Drawing.Size(190, 22);
+            this.txtProduct.TabIndex = 2;
+            // 
+            // txtSortedArray
+            // 
+            this.txtSortedArray.Location = new System.Drawing.Point(230, 216);
+            this.txtSortedArray.Name = "txtSortedArray";
+            this.txtSortedArray.Size = new System.Drawing.Size(192, 22);
+            this.txtSortedArray.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(36, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Сума (між нульовими ел.):";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(36, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Добуток:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(36, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Відсортовано:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(36, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Масив:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCalculate.Location = new System.Drawing.Point(39, 293);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(142, 34);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "Обрахувати";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, -1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(792, 452);
+            this.tabControl1.TabIndex = 9;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtArray);
+            this.tabPage1.Controls.Add(this.btnCalculate);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.txtProduct);
+            this.tabPage1.Controls.Add(this.txtSortedArray);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.txtSum);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(784, 423);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Завдання 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnCheck);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(784, 423);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Завдання 2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(312, 190);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(122, 31);
+            this.btnCheck.TabIndex = 3;
+            this.btnCheck.Text = "Перевірити";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(86, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(476, 25);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Чи є парні числа, у верхньому або нижньому куті?";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(86, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(651, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Чи є числа, що закінчуються нулем у верхньому або нижньому куті?";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtArray;
+        private System.Windows.Forms.TextBox txtSum;
+        private System.Windows.Forms.TextBox txtProduct;
+        private System.Windows.Forms.TextBox txtSortedArray;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR_18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double[] arr = Array.ConvertAll(txtArray.Text.Split(','), double.Parse);
+
+            double product = 1;
+            for (int i = 0; i < arr.Length; i += 2)
+            {
+                product *= arr[i];
+            }
+
+            double sum = 0;
+            int firstZeroIndex = Array.IndexOf(arr, 0);
+            int lastZeroIndex = Array.LastIndexOf(arr, 0);
+            if (firstZeroIndex >= 0 && lastZeroIndex >= 0 && firstZeroIndex != lastZeroIndex)
+            {
+                for (int i = firstZeroIndex + 1; i < lastZeroIndex; i++)
+                {
+                    sum += arr[i];
+                }
+            }
+
+            double[] positiveArr = arr.Where(x => x >= 0).ToArray();
+            double[] negativeArr = arr.Where(x => x < 0).ToArray();
+            Array.Sort(positiveArr);
+            Array.Reverse(positiveArr);
+            Array.Sort(negativeArr);
+            double[] sortedArr = positiveArr.Concat(negativeArr).ToArray();
+
+            txtProduct.Text = product.ToString();
+            txtSum.Text = sum.ToString();
+            txtSortedArray.Text = string.Join(", ", sortedArr);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[,] matrix = new int[,]
+    {
+        { 1, 2, 3, 4 },
+        { 5, 6, 7, 8 },
+        { 9, 10, 11, 12 },
+        { 13, 14, 15, 16 }
+    };
+
+            bool hasEvenNumberLeft = false;
+            bool hasNumberEndingWithZeroRight = false;
+
+            // Перевірка на наявність парних чисел у лівих кутах
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (matrix[i, 0] % 2 == 0 || matrix[i, matrix.GetLength(1) - 1] % 2 == 0)
+                {
+                    hasEvenNumberLeft = true;
+                    break;
+                }
+            }
+
+            // Перевірка на наявність чисел, що закінчуються нулем, у правих кутах
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (matrix[i, matrix.GetLength(1) - 1] % 10 == 0)
+                {
+                    hasNumberEndingWithZeroRight = true;
+                    break;
+                }
+            }
+
+            // Повідомлення про результат
+            if (hasEvenNumberLeft)
+            {
+                MessageBox.Show("Знайдено парне число у лівому куті");
+            }
+            else
+            {
+                MessageBox.Show("У лівому куті немає парних чисел");
+            }
+
+            if (hasNumberEndingWithZeroRight)
+            {
+                MessageBox.Show("Знайдено число, що закінчується на 0, у правому куті");
+            }
+            else
+            {
+                MessageBox.Show("У правому куті немає чисел, що закінчуються на 0");
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
